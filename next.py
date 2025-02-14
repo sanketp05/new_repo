2782167a1fb7bfd2607ffe7c9684fc98273c269d
sanketp05 <sanketpansuria05@gmail.com>
2025-02-14 11:26:44 +0530

    Commit in sanket3 branch

--- next.py
+++ next.py
@@ -1,6 +1,2 @@
-print('Sanket')
-print('Pansuriya Sanket')
-print('sanket')
-print("change in sanket3 branch")
-print('This is master node')
-print('This is master node"s line ')
+print("Sanket Pansuriya")
+print("This is sanket2 branch")
